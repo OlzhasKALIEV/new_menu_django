8c490486562cb50da89a4a3defda5b4c9d24c8eb
OlzhasKALIEV <130698888+OlzhasKALIEV@users.noreply.github.com>
2023-10-08 18:00:27 +0600

    menu

---
+++ menu_app/templatetags/common_tags.py
@@ -0,0 +1,11 @@
+from django import template
+from menu_app.models import MenuItem
+register = template.Library()
+
+
+@register.inclusion_tag('templatetags/menu.html', takes_context=True)
+def draw_menu(context):
+    menu_items = MenuItem.objects.filter(level=1)
+    return {
+        "menu_items": menu_items,
+    }
